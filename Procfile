59fbcd69aa8e3a053309e392028a1f0f0b40c5d6
alessandro <alcobplayer255.com@gmail.com>
2023-06-09 18:24:30 +0200

    bd

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn matematica.matematica.wsgi
+web: gunicorn matematica.wsgi
--- matematica/wsgi.py
+++ matematica/wsgi.py
@@ -16,4 +16,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "matematica.settings")
 application = get_wsgi_application()
 
 # per vercel: .vercel.app
-app = application
+# app = application
