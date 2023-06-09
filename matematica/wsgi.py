7a177a8e2ac5f5ebc2c9cea2f763273a37718815
alessandro <alcobplayer255.com@gmail.com>
2023-06-09 19:12:29 +0200

    bd

--- matematica/wsgi.py
+++ matematica/wsgi.py
@@ -12,8 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "matematica.matematica.settings")
-os.environ.setdefault("DJANGO_URLS_MODULE", "matematica.matematica.settings")
-os.environ.setdefault("DJANGO_ASGI_MODULE", "matematica.matematica.settings")
+os.environ.setdefault("DJANGO_URLS_MODULE", "matematica.matematica.urls")
+os.environ.setdefault("DJANGO_ASGI_MODULE", "matematica.matematica.asgi")
 
 application = get_wsgi_application()
 
